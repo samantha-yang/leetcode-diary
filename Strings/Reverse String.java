bd1656c5910773b950f6f658d4fcf0d8844c5425
Samantha Yang <samanthayang07@yahoo.com>
2026-02-18 22:26:18 -0800

    Reverse string

---
+++ Strings/Reverse String.java
@@ -0,0 +1,18 @@
+public class Solution
+    public String reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // Store the character of the left pointer
+            char temp = s[left];
+            // Set the char at the left pointer to be the one at the right pointer
+            s[left] = s[right];
+            // Set the char at the right pointer to be the one at the left
+            s[right] = temp;
+            // Increment and decrement left and right pointers
+            left++;
+            right--;
+        }
+
+    }
