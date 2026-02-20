96ef6396e85c0c36fdeadde007739fdb94d4cf69
Samantha Yang <samanthayang07@yahoo.com>
2026-02-19 16:31:35 -0800

    First occurrence index in string

---
+++ Strings/First Occurrence Index in String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int occurrenceIndex(String haystack, String needle) {
+        // If the length of the needle is longer than the haystack, return -1 since
+        // it will not occur in the haystack
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        // Iterate through the haystack and check if the chars from i to the length of
+        // the needle + i match the needle string
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, needle.length() + i).equals(needle)) {
+                return i;
+            }
+        }
+
+        // Return -1 if there are no matches
+        return -1;
+    }
+}
